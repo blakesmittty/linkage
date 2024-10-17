cfb7f68979839a5e0e07d3933e0a55fbf8849f19
Blake <blakeas12@gmail.com>
2024-10-16 21:50:44 -0400

    got the blocks in

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -5,6 +5,7 @@ import { useState, useRef } from "react";
 function Game() {
     const canvasRef = useRef(null);
     const appRef = useRef(null);
+    const blockToDropRef = useRef(null);
     
     useEffect(() => {
         const initPixi = async() => {
@@ -14,24 +15,11 @@ function Game() {
             await app.init({
                 width: 700,
                 height: 800,
-                backgroundColor: 0xffffff
+                backgroundColor: 'black'
             });
             canvasRef.current.appendChild(app.canvas);
             appRef.current = app;
-
-            /*
-            PIXI.Assets.add([
-                { alias: '1', src: '/sheets/1_block_sheet.json' },
-                { alias: '2', src: '/sheets/2_block_sheet.json' },
-                { alias: '3', src: '/sheets/3_block_sheet.json' },
-                { alias: '4', src: '/sheets/4_block_sheet.json' },
-                { alias: '5', src: '/sheets/5_block_sheet.json' },
-                { alias: '6', src: '/sheets/6_block_sheet.json' },
-                { alias: '7', src: '/sheets/7_block_sheet.json' },
-                { alias: 'barrier', src: '/sheets/barrier_block_sheet.json' }
-            ]);
-            */
-            //await PIXI.Assets.load(['1', '2', '3', '4', '5', '6', '7', 'barrier']);
+            console.log("loading assets");
 
             PIXI.Assets.addBundle('blocks', [
                 { alias: '1', src: '/sheets/1_block_sheet.json' },
@@ -45,18 +33,66 @@ function Game() {
             ]);
 
             const assets = await PIXI.Assets.loadBundle('blocks');
+            console.log('assets loaded');
+
+            const oneBlock = new PIXI.Sprite(assets['1'].textures['default']);
+            oneBlock.width = 100;
+            oneBlock.height = 100;
+            blockToDropRef.current = oneBlock;
+            
+
+
+            const oneBlockBreak = new PIXI.AnimatedSprite([
+                assets['1'].textures['frame1'],
+                assets['1'].textures['frame2'],
+                assets['1'].textures['frame3'],
+                assets['1'].textures['frame4']
+            ]);
+
+            app.stage.addChild(oneBlock);
+
+            // oneBlockBreak.width = 100;
+            // oneBlockBreak.height = 100;
+            // oneBlockBreak.x = 300;
+            // oneBlockBreak.y = 300;
+            // oneBlockBreak.animationSpeed = 0.2;
+            // oneBlockBreak.play();
+            // app.stage.addChild(oneBlock);
+            // app.stage.addChild(oneBlockBreak);
+
+            
+        
         };
         initPixi();
 
 
         return () => {
-            appRef.current.destroy(true, { children: true, texture: true, baseTexture: true });
-            appRef.current = null;
+            if (appRef.current) {
+                console.log("destroying pixi app");
+                appRef.current.destroy(true, { children: true, texture: true, baseTexture: true });
+                appRef.current = null;
+            }
         }
-    });
+    }, []);
+
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            switch (e.key) {
+                case 'ArrowLeft': blockToDropRef.current.x -= 100; break;
+                case 'ArrowRight': blockToDropRef.current.x += 100; break;
+                default: break;
+            }
+        }
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        }
+    }, []);
 
     return (
-        <canvas ref={canvasRef} />
+        <div ref={canvasRef} />
     );
     
 };
